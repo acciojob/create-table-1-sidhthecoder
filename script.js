e035f42be001554563449fa43410e6d79416999c
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-05-18 14:17:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+  var table = document.getElementById("sampleTable");
+  var newRow = table.insertRow(0);
+  var cell1 = newRow.insertCell(0);
+  var cell2 = newRow.insertCell(1);
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
 }
+
+
